1c76a0c6226b361c5243111faa4c5e658b117187
Garrett Hill <ghill012@gmail.com>
2020-07-04 17:59:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -6,13 +6,10 @@ end
 
 
 def state_log(star_date)
+    star_date = generate_star_date
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
   puts state_log
-  date = generate_star_date
-  end
-  binding.pry
-  puts"hello"
 end
